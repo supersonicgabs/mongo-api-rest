a2964d7d772eff9d70fc3e74ff83fcbf2de9558b
Gabriel Fonseca <gabrielfonseca@MacBook-Air-de-Gabriel.local>
2020-02-04 22:24:14 -0300

    ignoring database

--- config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.connect('mongodb+srv://supersonicgabs:bob@,fett182@cluster0-c8fq9.mongodb.net/rest-api?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
