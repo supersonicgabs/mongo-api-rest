ddd636c60742bd2898522ffea61632b42fbdbfcf
Gabriel Fonseca <gabrielfonseca@MacBook-Air-de-Gabriel.local>
2020-02-04 22:21:38 -0300

    gitignore

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb+srv://supersonicgabs:password@cluster0-c8fq9.mongodb.net/rest-api?retryWrites=true&w=majority', {
+module.exports = mongoose.connect('mongodb+srv://supersonicgabs:bob@,fett182@cluster0-c8fq9.mongodb.net/rest-api?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
